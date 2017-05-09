ff03d8c220edd9cb51f9a410d44778379dfe4386
David McGaughey <mcgaughey@gmail.com>
2017-05-09 15:42:40 -0400

    added mols 2 atoms function

--- conversion.py
+++ conversion.py
@@ -1,4 +1,5 @@
 # Steve is great!!!! 
+# Hello class
 def dollars_to_cents(dollars):
 	cents = dollars * 100
 	return cents
@@ -11,5 +12,7 @@ def gallons2liters(gallons):
 	liters = gallons * 3.78
 	return(liters)
 
-
+def moles2atoms(mol):
+	atoms = mols * 6.02e23
+	return(atoms)
 
