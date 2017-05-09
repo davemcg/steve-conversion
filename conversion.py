1889e0af8d9ecc9ca91bf6cfc60aacdd64ecf616
David McGaughey <mcgaughey@gmail.com>
2017-05-09 15:15:25 -0400

    added hours 2 minutes function

--- conversion.py
+++ conversion.py
@@ -1,7 +1,11 @@
-# Steve sucks!!!!
+# Steve is great!!!! 
 def dollars_to_cents(dollars):
 	cents = dollars * 100
 	return cents
+
+def hours2minutes(hours):
+	minutes = hours * 60
+	return(hours)
 
 
 
